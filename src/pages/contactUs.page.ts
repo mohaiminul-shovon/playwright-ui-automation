a8bf9510114456b9be7f040d06991a7cbc3922fb
mohaiminul-shovon <mi.shovon23@gmail.com>
2025-03-10 15:21:08 +0600

    test case 6 added

--- src/pages/contactUs.page.ts
+++ src/pages/contactUs.page.ts
@@ -1,7 +1,52 @@
-import {Page} from '@playwright/test'
+import {Locator, Page} from '@playwright/test'
+import * as path from 'path';
 export class ContactUsPage{
     readonly page: Page;
+    readonly pageHeading: Locator;
+    readonly contanctFormNameField: Locator;
+    readonly contanctFormEmailField: Locator;
+    readonly contactFormSubjectField: Locator;
+    readonly contactFormMessageField: Locator;
+    readonly contactFormUploadFileBtn: Locator;
+    readonly contactFormSubmitBtn: Locator;
+    readonly formHeading: Locator;
     constructor(page: Page){
         this.page = page;
+        this.pageHeading = this.page.getByRole('heading', { name: 'Contact Us' });
+        this.contanctFormNameField = this.page.getByRole('textbox', { name: 'Name' });
+        this.contanctFormEmailField = this.page.getByRole('textbox', { name: 'Email', exact: true });
+        this.contactFormSubjectField = this.page.getByRole('textbox', { name: 'Subject' });
+        this.contactFormMessageField = this.page.getByRole('textbox', { name: 'Your Message Here' });
+        this.contactFormUploadFileBtn = this.page.locator('input[type="file"]');
+        this.contactFormSubmitBtn = this.page.getByRole('button', { name: 'Submit' });
+        this.formHeading = this.page.getByRole('heading', { name: 'Get In Touch' });
+    }
+    async navigateToContactUsPage(){
+        await this.page.goto('https://automationexercise.com/contact_us');
+    }
+    async isPageHeadingVisible(){
+        await this.pageHeading.waitFor();
+    }
+    async fillContactFormAndSubmit(name: string, email: string, subject: string, message: string){
+        await this.contanctFormNameField.fill(name);
+        await this.contanctFormEmailField.fill(email);
+        await this.contactFormSubjectField.fill(subject);
+        await this.contactFormMessageField.fill(message);
+        await this.fileUploadForContactForm();
+        await this.contactFormSubmitBtn.click();
+    }
+
+    async fileUploadForContactForm(){
+        const fileChooserPromise = this.page.waitForEvent('filechooser');
+        await this.contactFormUploadFileBtn.click();
+        const fileChooser = await fileChooserPromise;
+        await fileChooser.setFiles(path.join('src/utils', 'test_file.png'));
+    }
+    async submitContactForm(){
+        await this.contactFormSubmitBtn.click();
+    }
+    async getSuccessMessage(){
+        const successMessage = await this.page.locator('#contact-page').getByText('Success! Your details have');
+        return successMessage;
     }
 }
--- src/tests/test_case6.spec.ts
+++ src/tests/test_case6.spec.ts
@@ -1,5 +1,7 @@
 import {test, expect} from '@playwright/test';
-import { HomePage } from '../pages/homepage.page'; 
+import { HomePage } from '../pages/homepage.page';
+import { ContactUsPage } from '../pages/contactUs.page'; 
+import exp from 'constants';
 
 test('Contact Us Form', async ({ page }) => {
     const homepage = await new HomePage(page);
@@ -8,4 +10,16 @@ test('Contact Us Form', async ({ page }) => {
     const nagivationPanel = await homepage.getNavPanel();
     const contactUsLink = await nagivationPanel.getNavElement('Contact Us');
     await contactUsLink.click();
+    const contactUsPage = await new ContactUsPage(page);
+    const formHeading = await contactUsPage.formHeading;
+    await expect(formHeading).toHaveText('Get In Touch');
+    page.on('dialog', async dialog => {
+        console.log(`Dialog message: ${dialog.message()}`);
+        await dialog.accept(); // or dialog.dismiss();
+      });
+    await contactUsPage.fillContactFormAndSubmit("shovon","shovon@gold.com","LOL LOL!", "Haghagafafa");
+    //await contactUsPage.submitContactForm();
+    const successMessage = await contactUsPage.getSuccessMessage();
+    await expect(successMessage).toHaveText('Success! Your details have been submitted successfully.');
+    
 });
